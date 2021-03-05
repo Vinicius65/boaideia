1cca83c0a75820397b0410e532666cc2a4263e86
Vinicius <Viniciusbss124@gmail.com>
2021-03-05 01:02:41 -0300

    add provides

---
+++ boaideia-backend/BoaIdeia.Api/Constants/Providers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.Constants
+{
+    public static class Providers
+    {
+        public const string GOOGLE = "GOOGLE";
+        public const string GITHUB = "GITHUB";
+    }
+}
