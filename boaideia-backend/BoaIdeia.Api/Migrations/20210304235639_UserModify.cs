47e89a92a680e838d9fbee4be53c4034f8ee112c
Vinicius <Viniciusbss124@gmail.com>
2021-03-05 01:03:12 -0300

    password allow null

---
+++ boaideia-backend/BoaIdeia.Api/Migrations/20210304235639_UserModify.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoaIdeia.Api.Migrations
+{
+    public partial class UserModify : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "user",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "user",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
