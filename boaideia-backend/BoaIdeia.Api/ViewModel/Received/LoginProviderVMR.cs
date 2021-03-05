8907484538be8f26e010645bb2daf4bd2f26138a
Vinicius <Viniciusbss124@gmail.com>
2021-03-05 01:04:27 -0300

    aply setter hash password to vw

---
+++ boaideia-backend/BoaIdeia.Api/ViewModel/Received/LoginProviderVMR.cs
@@ -0,0 +1,23 @@
+﻿using BoaIdeia.Api.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.ViewModel
+{
+    public class LoginProviderVMR
+    {
+        public string Name { get; set; }
+        public string SecretProvider { get; set; }
+        public string Github { get; set; }
+        public string GithubId { get; set; }
+
+        public string Stackoverflow { get; set; }
+        public string StackoverflowId { get; set; }
+
+        public string Google { get; set; }
+        public string GoogleId { get; set; }
+
+    }
+}
