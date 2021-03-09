a9d3e5346c164e5f0903c10e5f258f8b677cfc54
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 02:28:01 -0300

    create mapper extension
    mapper extension to convert complex objets

---
+++ boaideia-backend/BoaIdeia.Api/Extensions/MapperExtension.cs
@@ -0,0 +1,29 @@
+﻿using BoaIdeia.Api.Models;
+using BoaIdeia.Api.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.Extensions
+{
+    public static class MapperExtension
+    {
+        public static UserVM To_UserVM(this User user)
+        {
+            if (user is null)
+                return null;
+
+            return new UserVM()
+            {
+                Email = user.Email.Value,
+                Github = user.Github,
+                Id = user.Id,
+                Name = user.Name,
+                NumberOfVotation = user.SocialRank.NumberOfVotation,
+                Rank = user.SocialRank.Rank,
+                Stackoverflow = user.Stackoverflow,
+            };
+        }
+    }
+}
