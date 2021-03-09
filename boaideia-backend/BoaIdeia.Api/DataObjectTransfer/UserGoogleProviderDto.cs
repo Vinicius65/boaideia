a9eda5c0b383a1452afa5731924dc7ed51edcf1d
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 02:26:12 -0300

    UserGoogleProviderDto

---
+++ boaideia-backend/BoaIdeia.Api/DataObjectTransfer/UserGoogleProviderDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.DataObjectTransfer
+{
+    public class UserGoogleProviderDto
+    {
+        public string Iss { get; set; }
+        public string Azp { get; set; }
+        public string Aud { get; set; }
+        public string Sub { get; set; }
+        public string Email { get; set; }
+        public string Email_verified { get; set; }
+        public string At_hash { get; set; }
+        public string Iat { get; set; }
+        public string Exp { get; set; }
+        public string Alg { get; set; }
+        public string Kid { get; set; }
+        public string Typ { get; set; }
+
+    }
+}
