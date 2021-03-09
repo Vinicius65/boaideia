4b9f9720ae20d653a70fde05f3c1f6c9ba131bf8
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 02:30:11 -0300

    Create Strip Extension and test

---
+++ boaideia-backend/BoaIdeia.Api/Extensions/StripExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.Extensions
+{
+    public static class StripExtension
+    {
+        public static string Strip(this string str)
+        {
+            if (str is null)
+                return null;
+
+
+            StringBuilder sb = new StringBuilder();
+            var len = str.Length;
+
+            int startPosition = -1;
+            int stopPosition = -1;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (startPosition == -1 && !Char.IsWhiteSpace(str[i]))
+                {
+                    startPosition = i;
+                }
+
+                var lastI = len - i - 1;
+                if (stopPosition == -1 && !Char.IsWhiteSpace(str[lastI]))
+                {
+                    stopPosition = lastI;
+                }
+
+                if (startPosition != -1 && stopPosition != -1)
+                {
+                    for (int j = startPosition; j <= stopPosition; j++)
+                    {
+                        sb.Append(str[j]);
+                    }
+                    return sb.ToString();
+                }
+               
+            }
+            return "";
+        }
+    }
+}
---
+++ boaideia-backend/BoaIdeia.Test/Cenarios/ExtensionStripTest.cs
@@ -0,0 +1,38 @@
+﻿using BoaIdeia.Api.Extensions;
+using BoaIdeia.Api.Models;
+using BoaIdeia.Api.ValueObject;
+using BoaIdeia.Api.ViewModel;
+using BoaIdeia.Api.ViewModel.Received;
+using BoaIdeia.Test.Util;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BoaIdeia.Test.Cenarios
+{
+    public class ExtensionStripTest
+    {
+        [Theory]
+        [InlineData("Vinicius", "Vinicius")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData(" ", "")]
+        [InlineData("  ", "")]
+        [InlineData("   ", "")]
+        [InlineData("    ", "")]
+        [InlineData("    Vinicius", "Vinicius")]
+        [InlineData("Vinicius    ", "Vinicius")]
+        [InlineData("  Vinicius  ", "Vinicius")]
+        [InlineData(" Vinicius ", "Vinicius")]
+        [InlineData("      Vinicius  ", "Vinicius")]
+        [InlineData("  Vinicius      ", "Vinicius")]
+        [InlineData("  Vin   icius      ", "Vin   icius")]
+        [InlineData("  Vi  nici  us      ", "Vi  nici  us")]
+        [InlineData("  V i n i c i u s      ", "V i n i c i u s")]
+        public void StripTest(string original, string excepted)
+        {
+            var strTest = original.Strip();
+            Assert.True(strTest == excepted);
+        }
+    }
+}
