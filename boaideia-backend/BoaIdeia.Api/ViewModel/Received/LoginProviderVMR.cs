4817af0e99461a81cc8a831bdaf2a7f3460f1a3b
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 02:29:30 -0300

    simplify LoginProviderVMR

--- boaideia-backend/BoaIdeia.Api/ViewModel/Received/LoginProviderVMR.cs
+++ boaideia-backend/BoaIdeia.Api/ViewModel/Received/LoginProviderVMR.cs
@@ -9,15 +9,7 @@ namespace BoaIdeia.Api.ViewModel
     public class LoginProviderVMR
     {
         public string Name { get; set; }
-        public string SecretProvider { get; set; }
-        public string Github { get; set; }
-        public string GithubId { get; set; }
-
-        public string Stackoverflow { get; set; }
-        public string StackoverflowId { get; set; }
-
-        public string Google { get; set; }
-        public string GoogleId { get; set; }
-
+        public string Email { get; set; }
+        public string Token { get; set; }
     }
 }
