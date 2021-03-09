1d836a0b4ed3b32be32dc9d01769337cbc372c0d
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 02:25:03 -0300

    add google provider service

---
+++ boaideia-backend/BoaIdeia.Api/Services/GoogleProvider.cs
@@ -0,0 +1,37 @@
+﻿using BoaIdeia.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BoaIdeia.Api.DataObjectTransfer;
+using BoaIdeia.Api.Extensions;
+
+namespace BoaIdeia.Api.Services
+{
+    public class GoogleProvider : IGoogleProvider
+    {
+
+        private readonly HttpClient _request;
+        public GoogleProvider(HttpClient request)
+        {
+            _request = request;
+        }
+        public async Task<bool> Auth(string token, string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            var response = await _request.GetAsync($"https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={token}");
+            var userGoogle = JsonSerializer.Deserialize<UserGoogleProviderDto>(await response.Content.ReadAsStringAsync());
+
+            if (email.ToLower().Strip() == userGoogle.Email && bool.Parse(userGoogle.Email_verified))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ boaideia-backend/BoaIdeia.Api/Services/Interfaces/IGoogleProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoaIdeia.Api.Services.Interfaces
+{
+    public interface IGoogleProvider
+    {
+        public Task<bool> Auth(string token, string email);
+    }
+}
