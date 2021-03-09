317f1876a090827d0ea9ec1a154d56e9c4318d50
Vinicius <Viniciusbss124@gmail.com>
2021-03-09 05:59:26 -0300

    fix name cadastro

--- boaideia-backend/BoaIdeia.Api/ViewModel/Received/CadastroUserVMR.cs
+++ boaideia-backend/BoaIdeia.Api/ViewModel/Received/CadastroUserVMR.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace BoaIdeia.Api.ViewModel.Received
 {
-    public class CadstroUserVMR
+    public class CadastroUserVMR
     {
         public string Name { get; set; }
         public string Email { get; set; }
