f07a34788017391d81e46defdad038dd349cbf03
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2021-06-30 22:35:59 -0300

    swagger de novo

--- boaideia-backend/BoaIdeia.Api/Startup.cs
+++ boaideia-backend/BoaIdeia.Api/Startup.cs
@@ -95,7 +95,7 @@ namespace BoaIdeia.Api
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = "api/swagger";
             });
 
